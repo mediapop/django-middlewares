745bb0faad190e68a5078db96915105a7d5465b9
nmhung89 <nmhung89@gmail.com>
2016-05-03 12:37:47 +1000

    Throw a nicer exception if X-Forwarded-For isn't set

--- mediapop/middleware/realip.py
+++ mediapop/middleware/realip.py
@@ -1,4 +1,9 @@
+from django.core.exceptions import ImproperlyConfigured
+
 
 class RealIP(object):
     def process_request(self, request):
-        request.META['REMOTE_ADDR'] = request.META['HTTP_X_FORWARDED_FOR'].split(',')[0]
+        try:
+            request.META['REMOTE_ADDR'] = request.META['HTTP_X_FORWARDED_FOR'].split(',')[0]
+        except KeyError:
+            raise ImproperlyConfigured("RealIP middleware is active but X-Forwarded-For isn't set.")
