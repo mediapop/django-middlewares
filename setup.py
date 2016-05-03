a0fd9f9e79f541389b6c1bb1130bcf1c9e807e2e
nmhung89 <nmhung89@gmail.com>
2016-05-03 12:45:02 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='django-middlewares',
+    version='0.1',
+    url='https://github.com/mediapop/django-middlewares',
+    author='Hung / Media Pop',
+    author_email='hung@mediapop.co',
+    description='Django middlewares used for mediapops projects',
+    license='BSD',
+    long_description=open('README.md').read(),
+    packages=['django-middlewares'],
+    package_data={
+        '': ['README.md']
+    },
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+    ],
+)
